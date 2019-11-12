0992c0ff10f889e98d02d487753335fbabe30824
Kevin <kevin_huang31@yahoo.co.id>
2019-11-12 21:27:04 +0700

    initial

---
+++ SQL/SQLQuery.sql
@@ -0,0 +1,2 @@
+/*update Data set FirstName = 'John' where LastName = 'Frank'*/
+select * from Data
---
+++ SQL/SQLQuery3.sql
@@ -0,0 +1,11 @@
+/*insert into sales_detail(no_faktur, no_data, kode_produk, qty, harga, sub_total)
+values ('1', '1', '01', 1, 1000, 1000)
+insert into sales_detail(no_faktur, no_data, kode_produk, qty, harga, sub_total)
+values ('2', '2', '02', 1, 1000, 1000)
+insert into sales_detail(no_faktur, no_data, kode_produk, qty, harga, sub_total)
+values ('3', '3', '03', 1, 1000, 1000)
+insert into sales_detail(no_faktur, no_data, kode_produk, qty, harga, sub_total)
+values ('4', '4', '04', 1, 1000, 1000)
+insert into sales_detail(no_faktur, no_data, kode_produk, qty, harga, sub_total)
+values ('5', '5', '05', 1, 1000, 1000)*/
+select * from sales_detail
